2e2ed53f10bc5ce4401a1ceb205bbfa8d3b09df1
Alina <you@example.com>
2023-10-11 21:11:58 +0300

    commit two

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import Button from './Components/Button/Button';
 function App() {
   return (
     <div>
       <h1>Hi I`m shopbot</h1>
+      <Button type={"add"} title={"+"} disable={false} />
+      <Button type={"remove"} title={"-"} disable={false} />
+      <Button type={"checkout"} title={"checkout"} disable={false} />
     </div>
   );
 }
---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+import pizza from "../images/pizza.png"
+import burger from "../images/burger.png"
+import coca from "../images/coca.png"
+import icecream from "../images/icecream.png"
+import kebab from "../images/kebab.png"
+import salad from "../images/salad.png"
+import water from "../images/water.png"
+
+function getData()
+{
+    return[
+        {title:"Pizza", price: 17.99, Image: pizza, id:1},
+        {title:"Burger", price: 20.99, Image: burger, id:2},
+        {title:"Coca", price: 3, Image: coca, id:3},
+        {title:"IceCream", price: 10.28, Image: icecream, id:4},
+        {title:"Kebab", price: 18.80, Image: kebab, id:5},
+        {title:"Salat", price: 17.99, Image: salad, id:6},
+        {title:"Water", price: 0.17, Image: water, id:7}
+    ]
+}
